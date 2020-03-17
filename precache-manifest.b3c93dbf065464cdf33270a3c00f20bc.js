b2af984fbd37202cef04b83165b58a2bcf582a79
ikigamisama <kotascross22@gmail.com>
2020-03-17 17:49:25 +0800

    Updates

---
+++ precache-manifest.b3c93dbf065464cdf33270a3c00f20bc.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a010fe3df5d0e71d9ad87f16b3c920fd",
+    "url": "/react-redux-todo-app/index.html"
+  },
+  {
+    "revision": "0ffd8ea069779606c437",
+    "url": "/react-redux-todo-app/static/css/2.d103f7a6.chunk.css"
+  },
+  {
+    "revision": "e41d59e676fe67dc5dc2",
+    "url": "/react-redux-todo-app/static/css/main.38e71228.chunk.css"
+  },
+  {
+    "revision": "0ffd8ea069779606c437",
+    "url": "/react-redux-todo-app/static/js/2.9bd622f9.chunk.js"
+  },
+  {
+    "revision": "0a80d11ce330483c52028ec8ef3dd64d",
+    "url": "/react-redux-todo-app/static/js/2.9bd622f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e41d59e676fe67dc5dc2",
+    "url": "/react-redux-todo-app/static/js/main.0b040ae7.chunk.js"
+  },
+  {
+    "revision": "d8dd754e403555f7bb32",
+    "url": "/react-redux-todo-app/static/js/runtime-main.bac254be.js"
+  },
+  {
+    "revision": "c924155285cdba2f57cbd751674c1bb0",
+    "url": "/react-redux-todo-app/static/media/antd.c9241552.png"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/react-redux-todo-app/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/react-redux-todo-app/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/react-redux-todo-app/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/react-redux-todo-app/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/react-redux-todo-app/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/react-redux-todo-app/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/react-redux-todo-app/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/react-redux-todo-app/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/react-redux-todo-app/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/react-redux-todo-app/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/react-redux-todo-app/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "d2b1e19091a3530dfb8bec06b5a926dc",
+    "url": "/react-redux-todo-app/static/media/material-ui.d2b1e190.png"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/react-redux-todo-app/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/react-redux-todo-app/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/react-redux-todo-app/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/react-redux-todo-app/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/react-redux-todo-app/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "68f9d883e71d52602c99476fca4043a5",
+    "url": "/react-redux-todo-app/static/media/reactstrap.68f9d883.png"
+  }
+]);
